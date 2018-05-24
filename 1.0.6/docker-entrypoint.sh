3decd484b826a5ad6da45e91c855d06b18452f16
abhishek <abhishek.tripathi@inmobi.com>
2018-05-24 16:59:54 +0530

    graphite prefix changs

--- 1.0.6/docker-entrypoint.sh
+++ 1.0.6/docker-entrypoint.sh
@@ -3,10 +3,10 @@
 set -e
 
 # Allow the container to be started with `--user`
-if [ "$1" = 'storm' -a "$(id -u)" = '0' ]; then
-    chown -R "$STORM_USER" "$STORM_CONF_DIR" "$STORM_DATA_DIR" "$STORM_LOG_DIR"
-    exec su-exec "$STORM_USER" "$0" "$@"
-fi
+#if [ "$1" = 'storm' -a "$(id -u)" = '0' ]; then
+#    chown -R "$STORM_USER" "$STORM_CONF_DIR" "$STORM_DATA_DIR" "$STORM_LOG_DIR"
+#    exec su "$STORM_USER" "$0" "$@"
+#fi
 
 STORM_CONF_FILE=${STORM_CONF_DIR}/storm.yaml
 COLLECTD_CONF_FILE=/tmp/collectd.conf
@@ -19,26 +19,28 @@ sed -i "s/ZK3/${ZK3}/g" ${STORM_CONF_FILE}
 sed -i "s/ZK_PORT/${ZK_PORT}/g" ${STORM_CONF_FILE}
 sed -i "s|DATA_DIR|${STORM_DATA_DIR}|g" ${STORM_CONF_FILE}
 sed -i "s|LOG_DIR|${STORM_LOG_DIR}|g" ${STORM_CONF_FILE}
-#cp /etc/collectd/collectd.conf /tmp
-#sed -i "s|HOST_NAME|${privateIp}|g" ${COLLECTD_CONF_FILE}
-#if [[ ! -z "GRAPHITE_HOST" ]]; then
-#	sed -i "s|GRAPHITE_HOST|${GRAPHITE_HOST}|g" ${COLLECTD_CONF_FILE}
-#	sed -i "s|GRAPHITE_PORT|${GRAPHITE_PORT}|g" ${COLLECTD_CONF_FILE}
-#fi
-#rm -f /etc/collectd/collectd.conf
-#/usr/sbin/collectdmon -P /var/run/collectd.pid -- -C ${COLLECTD_CONF_FILE}
-#collectd -C /tmp/collectd.conf -f &
-cores=$(nproc --all)
-python ${STORM_CONF_DIR}/yaml_edit.py $STORM_CONF_FILE $cores
-# Generate the config only if it doesn't exist
-#CONFIG="$STORM_CONF_DIR/storm.yaml"
-#if [ ! -f "$CONFIG" ]; then
-#    cat << EOF > "$CONFIG"
-#storm.zookeeper.servers: [zookeeper]
-#nimbus.seeds: [nimbus]
-#storm.log.dir: "$STORM_LOG_DIR"
-#storm.local.dir: "$STORM_DATA_DIR"
-#EOF
-#fi
+cp /etc/collectd/collectd.conf /tmp
+sed -i "s|HOST_NAME|${privateIp}|g" ${COLLECTD_CONF_FILE}
+collectdPrefix="$(echo $privateIp | sed -r 's/\./-/g')"
+nimbusPrefix="$(echo $NIMBUS1 |sed -r 's/\./-/g')"
+supervisorPrefix="$(echo $privateIp | sed -r 's/\./-/g')"
 
+if [[ ! -z "GRAPHITE_HOST" ]]; then
+	sed -i "s|GRAPHITE_HOST|${GRAPHITE_HOST}|g" ${COLLECTD_CONF_FILE}
+	sed -i "s|GRAPHITE_PORT|${GRAPHITE_PORT}|g" ${COLLECTD_CONF_FILE}
+        sed -i "s|INSTALL_TAG|$INSTALL_TAG|g" ${COLLECTD_CONF_FILE}
+	sed -i "s|HOST_IP|$collectdPrefix|g" ${COLLECTD_CONF_FILE}
+	sed -i "s|GRAPHITE_HOST|${GRAPHITE_HOST}|g" ${JMX_CONF_DIR}/*
+	sed -i "s|GRAPHITE_PORT|${GRAPHITE_PORT}|g" ${JMX_CONF_DIR}/*
+        sed -i "s|SUPERVISOR_IP|$supervisorPrefix|g" ${JMX_CONF_DIR}/*
+	sed -i "s|NIMBUS_IP|$nimbusPrefix|g" ${JMX_CONF_DIR}/*
+	sed -i "s|INSTALL_TAG|$INSTALL_TAG|g" ${JMX_CONF_DIR}/*
+	/etc/init.d/jmxtrans start
+        collectd -C /tmp/collectd.conf -f &
+fi
+#cores=$(nproc --all)
+cores=$(($(nproc --all)/4))
+if [ $cores -eq 0 ]
+   cores=1
+python ${STORM_CONF_DIR}/yaml_edit.py $STORM_CONF_FILE $cores
 exec "$@"
